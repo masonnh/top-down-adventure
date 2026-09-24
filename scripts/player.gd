da6acc9ef2de512807225463c940fc4d7876d172
Mason Hunter <masonlizard@gmail.com>
2026-09-24 15:47:51 -0400

    base level, slime health

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ const SPEED = 300.0
 var last_direction: Vector2 = Vector2.RIGHT
 var is_attacking: bool = false
 var hitbox_offset: Vector2
+var strength: int = 20
 
 
 func _ready() -> void:
@@ -102,3 +103,4 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 	if is_attacking and body.name.begins_with("Slime"):
 		print(body.name)
 		print("Hit")
+		body.take_damage(strength, position)
---
+++ scripts/slime.gd
@@ -0,0 +1,68 @@
+extends CharacterBody2D
+@onready var anim_spr: AnimatedSprite2D = $AnimatedSprite2D
+@onready var take_damage_sound: AudioStreamPlayer2D = $TakeDamage
+@onready var health_bar: Node2D = $HealthBar
+
+
+const SPEED = 100
+const KNOCKBACK_FORCE = 100
+
+var is_alive: bool = true
+var health : int = 100
+var target = null
+
+func _physics_process(delta: float) -> void:
+	if !is_alive:
+		return
+	
+	if target:
+		_attack(delta)
+
+
+func _attack(delta: float) -> void:
+	var direction = (target.position - position).normalized()
+	position += direction * SPEED * delta
+	anim_spr.play("attack")
+
+
+func take_damage(damage: int, attacker_position: Vector2) -> void:
+	health -= damage
+	health_bar.update_health(health)
+	
+	if health <= 0:
+		_die()
+		return
+	
+	take_damage_sound.play()
+	
+	# Knockback
+	var knockback_direction = (position - attacker_position).normalized()
+	var target_position = position + knockback_direction * KNOCKBACK_FORCE
+	
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "position", target_position, 0.5)
+
+
+func _die() -> void:
+	is_alive = false
+	anim_spr.play("die")
+	
+	take_damage_sound.pitch_scale = 0.5
+	take_damage_sound.play()
+	
+	# Disable collision
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sight/CollisionShape2D.set_deferred("disabled", true)
+
+
+func _on_sight_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		target = body
+
+
+func _on_sight_body_exited(body: Node2D) -> void:
+	if body.name == "Player" and is_alive:
+		target = null
+		anim_spr.play("idle")
